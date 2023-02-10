b6dde6e445aca530abbdf24caffda7aa79a276c7
rhkddms3501 <rhkddms3501@naver.com>
2023-02-10 15:41:07 +0900

    DVDPlayer test

--- container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
@@ -8,7 +8,6 @@ import com.douzone.container.videosystem.DVDPlayer;
 import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @Configuration
-
 public class DVDPlayerConfig {
 	
 	@Bean
@@ -19,10 +18,27 @@ public class DVDPlayerConfig {
 	// 주입(Injection)하기1
 	// Bean 생성 메소드를 직접 호출하는 방법
 	// 생성자 주입
-	@Bean
+	@Bean("dvdPlayer")
 	public DVDPlayer dvdPlayer01() {
 		return new DVDPlayer(avengers());
 	}
-	
-	
+
+	// 주입(Injection)하기2
+	// Parameter로 bean을 전달하는 방법
+	// 생성자 주입
+	@Bean
+	public DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+
+	// 주입(Injection)하기2
+	// Parameter로 bean을 전달하는 방법
+	// setter 주입
+	@Bean
+	public DVDPlayer dvdPlayer03(DigitalVideoDisc dvd) {
+		DVDPlayer dvdPlayer = new DVDPlayer();
+		dvdPlayer.setDvd(dvd);
+		
+		return dvdPlayer;
+	}
 }
